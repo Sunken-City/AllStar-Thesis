02dfaf83ab4f0977554a1cc13de8478a17ab49e5
Anthony Cloudy <acloudy@mail.smu.edu>
2019-01-12 14:21:33 -0800

    Cleanup for release

--- Code/Game/GameCommon.cpp
+++ Code/Game/GameCommon.cpp
@@ -1,14 +1,14 @@
 #include "Game\GameCommon.hpp"
 
 int g_frameNumber  = 0;
-bool g_renderDebug = true;
+bool g_renderDebug = false;
 bool g_isGamePaused = false;
 
 bool g_spawnEnemies             = true;
 bool g_spawnCrates              = true;
 bool g_spawnGeometry            = true;
 bool g_nearlyInvulnerable       = false;
-bool g_spawnWithDebugLoadout    = true;
+bool g_spawnWithDebugLoadout    = false;
 bool g_disableMusic             = false;
 
 const size_t gEffectTimeUniform = std::hash<std::string>{}("gEffectTime");
